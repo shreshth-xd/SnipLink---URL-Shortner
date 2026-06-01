c447c8065893fc35be834b5804f9deb38696473e
Shreshth Kanchan <shreshthkanchan777@gmail.com>
2026-06-01 18:00:48 +0530

    Added input validation and logged-in user url generation

--- app/api/shorten/route.js
+++ app/api/shorten/route.js
@@ -1,31 +1,57 @@
 import { NextResponse } from "next/server";
 import pool from "@/lib/db";
+import {getCurrentUser} from "@/lib/auth";
 
 export async function POST(req){
-
-    const body =
-        await req.json();
-
-    const {
-        originalUrl,
-        shortCode
-    } = body;
-
-    await pool.query(
-        `
-        INSERT INTO urls(
-            original_url,
-            short_code
-        )
-        VALUES ($1,$2)
-        `,
-        [
+    try{
+        
+        const user = await getCurrentUser();
+        const userId = user?.userId ?? null;
+    
+        const body =
+            await req.json();
+    
+            
+        const {
             originalUrl,
             shortCode
-        ]
-    );
+        } = body;
+    
 
-    return NextResponse.json({
-        success:true
-    });
+        if (!originalUrl || !shortCode) {
+            return NextResponse.json(
+                { error: "Missing required fields" },
+                { status: 400 }
+            );
+        }
+
+        await pool.query(
+            `
+            INSERT INTO urls(
+                original_url,
+                short_code,
+                user_id
+            )
+            VALUES ($1,$2,$3)
+            `,
+            [
+                originalUrl,
+                shortCode,
+                userId
+            ]
+        );
+    
+        return NextResponse.json({
+            success:true
+        });
+    }catch (error) {
+        console.error("Shorten error:", error);
+
+        return NextResponse.json(
+            { error: "Internal server error" },
+            { status: 500 }
+        );
+    }
+
+
 }
