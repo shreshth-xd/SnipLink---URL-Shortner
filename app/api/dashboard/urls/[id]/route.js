cc8aca91d464ed5fcdb4c07ead887cacd6a13b50
shreshth-xd <shreshthkanchan777@gmail.com>
2026-06-05 22:38:31 +0530

    Revising the soft deletion (UPDATE) query so that only live URLs can be soft deleted

--- app/api/dashboard/urls/[id]/route.js
+++ app/api/dashboard/urls/[id]/route.js
@@ -31,9 +31,12 @@ export async function DELETE(
             await pool.query(
                 `
                 UPDATE urls
-                SET deleted_at = NOW()
+                SET
+                    deleted_at = NOW(),
+                    updated_at = NOW()
                 WHERE id = $1
                 AND user_id = $2
+                AND deleted_at IS NULL
                 RETURNING id
                 `,
                 [
